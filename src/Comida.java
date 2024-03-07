7a9162acf4801d4efe98f90e0fe6c8a3715c2902
Gustavo Mariz Andrade <1380099@sga.pucminas.br>
2024-03-06 22:10:16 -0300

    Atualizado Valores Adicionais

--- src/Comida.java
+++ src/Comida.java
@@ -91,9 +91,12 @@ public class Comida {
         adicionarIngredientes(qtdExtras);
     }
 
-    //TODO
+    /**
+     * Calcula o valor dos ingredientes adicionais da comida.
+     * @return Valor (double) dos ingredientes para ser somados no preço
+    */
 	private double valorDosAdicionais() {
-		return 0;
+		return qtdAdicionais * valorPorAdicional;
 	}
     
     /**
